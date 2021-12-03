8736eabaa0a5de184645b261b2c5b3ba6ba9f4ae
Niels de Vries <nielsdevries@riseup.net>
2021-12-03 13:32:53 +0100

    fix sed

--- squid.sh
+++ squid.sh
@@ -6,8 +6,8 @@ dnf update -y
 dnf install -y squid
 
 # Write config file changes
-sed -i "18 acl blockedsites dstdomain \"/etc/squid/restricted-sites.squid\"" /etc/squid/squid.conf
-sed -i "19 http_access deny blockedsites" /etc/squid/squid.conf
+sed -i "18i acl blockedsites dstdomain \"/etc/squid/restricted-sites.squid\"" /etc/squid/squid.conf
+sed -i "19i http_access deny blockedsites" /etc/squid/squid.conf
 
 # Insert blocklist
 cat >/etc/squid/restricted-sites.squid <<EOL
