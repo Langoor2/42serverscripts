944183c3192d8b897fb31e600e00a3773986a5d2
Niels de Vries <nielsdevries@riseup.net>
2021-12-08 23:00:32 +0100

    Cleanup the ntp script

--- ntp.sh
+++ ntp.sh
@@ -6,14 +6,15 @@ dnf update -y
 # Install packages
 dnf -y install chrony
 
-allow 10.100.0.0/16 >> /etc/chrony.conf
+# Write the config file to allow servers to access the server
+cat >/etc/chrony.conf <<EOL
+allow 10.100.0.0/16
+EOL
 
-
-
-
-
+# Allow access through the firewall
 firewall-cmd --permanent --add-service=ntp
 firewall-cmd --reload
 
+# Start and enable the service at bootup
 systemctl enable chronyd
 systemctl start chronyd
