2aad824d6aeacfe49c05e2ee7e91343a76d1f0f2
Rick <rick537@live.nl>
2021-12-09 13:10:10 +0100

    Add files via upload
    Comments added.

--- splunk.sh
+++ splunk.sh
@@ -1,17 +1,20 @@
+#Installeren dnf
 sudo dnf install wget -y
 
 
+#Splunk configuratie
 groupadd splunk
 useradd -d /opt/splunk -m -g splunk splunk
 su - splunk
 id
 getconf LONG_BIT
 
-
+#Downloaden splunk - versie 8.1.0
 wget https://download.splunk.com/products/splunk/releases/8.1.0/linux/splunk-8.1.0-f57c09e87251-Linux-x86_64.tgz
 
-
+#Uitpakken splunk
 tar xvzf splunk-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
+
 cd /
 ls
 cd opt
@@ -29,12 +32,12 @@ su
 <wachtwoord>
 sudo ./splunk enable boot-start
 
-
-http://10.100.0.6:8000
+#Splunk configuratie Firewall
 
 firewall-cmd --zone=public --permanent --add-port 8080/tcp
 firewall-cmd --zone=public --permanent --add-port 8000/tcp
 firewall-cmd --zone=public --permanent --add-port 5514/udp
 firewall-cmd --reload
 
+#Server opnieuw starten
 reboot
