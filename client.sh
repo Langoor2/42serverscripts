4c16691bac12d9ed78b2428b34ef7ed8adfd1b6a
Niels de Vries <nielsdevries@riseup.net>
2021-12-09 15:02:39 +0100

    fix ntp

--- client.sh
+++ client.sh
@@ -9,13 +9,19 @@ sed -i 's#security.ubuntu.com#old-releases.ubuntu.com#' /etc/apt/sources.list
 # Update system
 
 apt update -y && apt upgrade -y
-apt install firefox thunderbird p7zip libreoffice sssd-ad sssd-tools realmd adcli
+apt -y install firefox thunderbird p7zip libreoffice sssd-ad sssd-tools realmd adcli ntp
+
+sed -i 's/pool/#pool/' /etc/ntp.conf
+echo 'server 10.100.0.10' >> /etc/ntp.conf
 
 # Join domain
 realm join treecko.ijselu.local
 
 # Enable home dir creation
 pam-auth-update --enable mkhomedir
+
+# Disable user list in gnome display manager
+sed -i 's@# disable-user-list=true@disable-user-list=true@g' /etc/gdm3/greeter.dconf-defaults
 
 # Mount /home on NFS
 10.100.0.9:/srv/homefolders /home nfs defaults 0 0 >> /etc/fstab
