c2b730478bb74a91c89f37d0c68aaa3fd672d2ed
Niels de Vries <nielsdevries@riseup.net>
2021-12-09 17:24:31 +0100

    add proxy settings

--- client.sh
+++ client.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-# Set DNS, IP, Proxy, NTP
-# IP and DNS is set by DHCP.
-
 # Fix repositorys
 sed -i 's#nl.archive.ubuntu.com#old-releases.ubuntu.com#' /etc/apt/sources.list
 sed -i 's#security.ubuntu.com#old-releases.ubuntu.com#' /etc/apt/sources.list
 
 # Update system
-
 apt update -y && apt upgrade -y
 apt -y install firefox thunderbird p7zip libreoffice sssd-ad sssd-tools realmd adcli ntp nfs-common
 
+# Set DNS, IP, Proxy, NTP
+# IP and DNS is set by DHCP.
 sed -i 's/pool/#pool/' /etc/ntp.conf
 echo 'server 10.100.0.10' >> /etc/ntp.conf
+
+echo 'http_proxy="http://10.100.0.7:3128/"' >> /etc/environment
+echo 'https_proxy="http://10.100.0.7:3128/"' >> /etc/environment
+echo 'no_proxy="localhost,127.0.0.1,::1"'
+touch /etc/apt/apt.conf.d/proxy.conf
+echo 'Acquire::http::Proxy "http://10.100.0.7:3128/";' >> /etc/apt/apt.conf.d/proxy.conf
+echo 'Acquire::https::Proxy "http://10.100.0.7:3128/";' >> /etc/apt/apt.conf.d/proxy.conf
 
 # Join domain
 echo 'cisco06' | realm join -U waltervl treecko.ijselu.local
