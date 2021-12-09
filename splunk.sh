f8e271bd420f917dbbb1d0fb4575c7b987b33f5e
Rick <rick537@live.nl>
2021-12-09 15:59:05 +0100

    Add files via upload

--- splunk.sh
+++ splunk.sh
@@ -1,6 +1,4 @@
-#Uitschakelen Selinux
-setenforce 0
-sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
+#!/bin/bash
 
 #Installeren dnf
 dnf install wget -y
@@ -9,9 +7,6 @@ dnf install wget -y
 #Splunk configuratie
 groupadd splunk
 useradd -d /opt/splunk -m -g splunk splunk
-su - splunk
-id
-getconf LONG_BIT
 
 #Downloaden splunk - versie 8.1.0
 wget https://download.splunk.com/products/splunk/releases/8.1.0/linux/splunk-8.1.0-f57c09e87251-Linux-x86_64.tgz
@@ -19,17 +14,6 @@ wget https://download.splunk.com/products/splunk/releases/8.1.0/linux/splunk-8.1
 #Uitpakken splunk
 tar xvzf splunk-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
 
-cd /
-ls
-cd opt
-ls
-cd splunk
-ls
-chown -R splunk: /opt/splunk/
-ls
-cd /
-cd opt/splunk/bin
-ls
 ./splunk start --accept-license --answer-yes --no-prompt --seed-passwd Cisco06!
 
 sudo ./splunk enable boot-start
