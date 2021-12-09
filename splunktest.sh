9bf964a847a0223c7862dfa8bfccfb268c07f82e
Rick <rick537@live.nl>
2021-12-09 17:36:24 +0100

    Add files via upload

--- splunktest.sh
+++ splunktest.sh
@@ -17,3 +17,6 @@ getconf LONG_BIT
 
 #Downloaden splunk - versie 8.1.0
 wget https://download.splunk.com/products/splunk/releases/8.1.0/linux/splunk-8.1.0-f57c09e87251-Linux-x86_64.tgz
+
+#Uitpakken splunk
+tar xvzf splunk-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
