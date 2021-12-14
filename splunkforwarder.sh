ae0c7cdfea0eb68297aac306d3fe57738fcdc311
Rick <rick537@live.nl>
2021-12-14 09:49:54 +0100

    Firewall Exceptions added

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -10,9 +10,17 @@ sudo wget https://download.splunk.com/products/universalforwarder/releases/8.1.0
 sudo tar xvzf splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
 
 #Configure Forwarder
-sudo /opt/splunkforwarder/bin/splunk --accept-license --answer-yes
+sudo /opt/splunkforwarder/bin/splunk start --accept-license --answer-yes -- no-prompt --seed-passwd Cisco06!
 sudo /opt/splunkforwarder/bin/splunk enable boot-start -user serviceaccount
 sudo /opt/splunkforwarder/bin/splunk add user admin -password Cisco06! -role admin
 sudo /opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
 sudo /opt/splunkforwarder/bin/splunk enable deploy-client
+
+#Firewall Config
+firewall-cmd --zone=public --permanent --add-port 8080/tcp
+firewall-cmd --zone=public --permanent --add-port 8000/tcp
+firewall-cmd --zone=public --permanent --add-port 5514/udp
+firewall-cmd --reload
+
+#Splunk Services Restart
 sudo /opt/splunkforwarder/bin/splunk restart
