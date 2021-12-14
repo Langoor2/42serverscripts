33d8870221b779f2234d617d1efdaea27816e755
Rick <rick537@live.nl>
2021-12-14 09:28:23 +0100

    Buf fixes.

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+#Update system
+sudo apt update
+
 #Download Universal Forwarder
-https://download.splunk.com/products/universalforwarder/releases/8.1.0/linux/splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz
+sudo wget https://download.splunk.com/products/universalforwarder/releases/8.1.0/linux/splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz
 
 #Extract
-tar xvzf /tmp/splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
+sudo tar xvzf splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
 
 #Configure Forwarder
-/opt/splunkforwarder/bin/splunk --accept-license --no-prompt --answer-yes enable boot-start -user serviceaccount
-/opt/splunkforwarder/bin/splunk add user admin -password Cisco06! -role admin
-/opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
+sudo /opt/splunkforwarder/bin/splunk --accept-license --no-prompt --answer-yes enable boot-start -user serviceaccount
+sudo /opt/splunkforwarder/bin/splunk add user admin -password Cisco06! -role admin
+sudo /opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
+sudo /opt/splunkforwarder/bin/splunk enable deploy-client
+sudo /opt/splunkforwarder/bin/splunk restart
