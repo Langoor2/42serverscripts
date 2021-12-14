6b972dac9ad221a49ea53452c8d2e129d08224a4
Rick <rick537@live.nl>
2021-12-14 09:43:31 +0100

    Minor adjustments

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -10,7 +10,8 @@ sudo wget https://download.splunk.com/products/universalforwarder/releases/8.1.0
 sudo tar xvzf splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
 
 #Configure Forwarder
-sudo /opt/splunkforwarder/bin/splunk --accept-license --no-prompt --answer-yes enable boot-start -user serviceaccount
+sudo /opt/splunkforwarder/bin/splunk --accept-license --answer-yes
+sudo /opt/splunkforwarder/bin/splunk enable boot-start -user serviceaccount
 sudo /opt/splunkforwarder/bin/splunk add user admin -password Cisco06! -role admin
 sudo /opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
 sudo /opt/splunkforwarder/bin/splunk enable deploy-client
