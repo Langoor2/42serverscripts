44fc348c6ab8231bae5161fdd7885e39456a1613
Rick <rick537@live.nl>
2021-12-16 08:32:37 +0100

    Updated Firewall Configuration

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -3,25 +3,30 @@
 #Update system
 sudo apt update
 
-sudo apt install firewalld -y
 
 #Download Universal Forwarder
 sudo wget https://download.splunk.com/products/universalforwarder/releases/8.1.0/linux/splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz
 
+
 #Extract
 sudo tar xvzf splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
 
+
 #Configure Forwarder
-sudo /opt/splunkforwarder/bin/splunk --accept-license --no-prompt --answer-yes enable boot-start -user admin
-sudo /opt/splunkforwarder/bin/splunk add user admin -password Cisco06! -role admin
-sudo /opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
+sudo /opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd Cisco06! enable boot-start
+sudo /opt/splunkforwarder/bin/splunk enable boot-start
+#sudo /opt/splunkforwarder/bin/splunk add user admin -password Cisco06! -role admin
 sudo /opt/splunkforwarder/bin/splunk enable deploy-client
+sudo /opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
+
 
 #Firewall Config
-sudo firewall-cmd --zone=public --permanent --add-port 8080/tcp
-sudo firewall-cmd --zone=public --permanent --add-port 8000/tcp
-sudo firewall-cmd --zone=public --permanent --add-port 5514/udp
-sudo firewall-cmd --reload
+sudo ufw enable
+sudo ufw allow 8080
+sudo ufw allow 8000
+sudo ufw allow 5514
+sudo ufw reload
+
 
 #Splunk Services Restart
 sudo /opt/splunkforwarder/bin/splunk restart
