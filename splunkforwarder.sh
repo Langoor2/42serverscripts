0a0d306dc14f8e6eb0c2cdff773c748780f72d03
Rick <rick537@live.nl>
2021-12-16 08:39:14 +0100

    Changed boot-start

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -14,7 +14,6 @@ sudo tar xvzf splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
 
 #Configure Forwarder
 sudo /opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd Cisco06! enable boot-start
-sudo /opt/splunkforwarder/bin/splunk enable boot-start
 #sudo /opt/splunkforwarder/bin/splunk add user admin -password Cisco06! -role admin
 sudo /opt/splunkforwarder/bin/splunk enable deploy-client
 sudo /opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
@@ -30,3 +29,6 @@ sudo ufw reload
 
 #Splunk Services Restart
 sudo /opt/splunkforwarder/bin/splunk restart
+
+#Splunk Configure Boot Start
+sudo /opt/splunkforwarder/bin/splunk enable boot-start
