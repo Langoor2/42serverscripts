a95581ce5149421f58ba4028bde44aca474af81b
Rick <rick537@live.nl>
2021-12-21 15:42:40 +0100

    Bug fixes

--- splunktest.sh
+++ splunktest.sh
@@ -21,25 +21,24 @@ wget https://download.splunk.com/products/splunk/releases/8.1.0/linux/splunk-8.1
 #Uitpakken splunk
 tar xvzf splunk-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
 
-cd /
-ls
-cd opt
-ls
-cd splunk
-ls
-chown -R splunk: /opt/splunk/
-ls
-cd /
-cd opt/splunk/bin
-ls
-
-./splunk start --accept-license --answer-yes --no-prompt --seed-passwd Cisco06!
-
-sudo ./splunk enable boot-start
 
 #Splunk configuratie Firewall
-
 firewall-cmd --zone=public --permanent --add-port 8080/tcp
 firewall-cmd --zone=public --permanent --add-port 8000/tcp
 firewall-cmd --zone=public --permanent --add-port 5514/udp
+firewall-cmd --zone=public --permanent --add-port 9997/udp
+firewall-cmd --zone=public --permanent --add-port 9997/tcp
 firewall-cmd --reload
+
+
+cd /
+cd opt/splunk/bin
+chown -R splunk: /opt/splunk/
+cd /
+cd opt/splunk/bin
+
+./splunk start --accept-license --answer-yes --no-prompt --seed-passwd Cisco06!
+
+./splunk enable boot-start
+./splunk enable listen 9997
+./splunk restart
