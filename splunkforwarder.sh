7b5c79a2c478b39477df5ee0944630077b09e0ca
Rick <rick537@live.nl>
2021-12-21 15:42:24 +0100

    Bug fixes

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -19,16 +19,25 @@ sudo /opt/splunkforwarder/bin/splunk enable deploy-client
 sudo /opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
 
 
+#Splunk Configure Boot Start
+sudo /opt/splunkforwarder/bin/splunk enable boot-start
+
+
 #Firewall Config
 sudo ufw enable
 sudo ufw allow 8080
 sudo ufw allow 8000
 sudo ufw allow 5514
+sudo ufw allow 9997
 sudo ufw reload
+
+
+#Splunk add monitoring logs
+sudo /opt/splunkforwarder/bin/splunk add forward-server 10.100.0.6:9997
+sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/auth.log
+sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/kern.log
+sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/lastlog
 
 
 #Splunk Services Restart
 sudo /opt/splunkforwarder/bin/splunk restart
-
-#Splunk Configure Boot Start
-sudo /opt/splunkforwarder/bin/splunk enable boot-start
