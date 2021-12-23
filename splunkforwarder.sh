ea238fa2fdc26a2136e5e48e6650cd41167d18e3
Rick <rick537@live.nl>
2021-12-23 14:19:17 +0100

    Removed Sudo commands, removed commented lines, removed apt update.

--- splunkforwarder.sh
+++ splunkforwarder.sh
@@ -1,43 +1,39 @@
 #!/bin/sh
 
-#Update system
-sudo apt update
-
 
 #Download Universal Forwarder
-sudo wget https://download.splunk.com/products/universalforwarder/releases/8.1.0/linux/splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz
+wget https://download.splunk.com/products/universalforwarder/releases/8.1.0/linux/splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz
 
 
 #Extract
-sudo tar xvzf splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
+tar xvzf splunkforwarder-8.1.0-f57c09e87251-Linux-x86_64.tgz -C /opt
 
 
 #Configure Forwarder
-sudo /opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd Cisco06! enable boot-start
-#sudo /opt/splunkforwarder/bin/splunk add user admin -password Cisco06! -role admin
-sudo /opt/splunkforwarder/bin/splunk enable deploy-client
-sudo /opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
+./opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd Cisco06! enable boot-start
+./opt/splunkforwarder/bin/splunk enable deploy-client
+./opt/splunkforwarder/bin/splunk set deploy-poll "10.100.0.6:8000"
 
 
 #Splunk Configure Boot Start
-sudo /opt/splunkforwarder/bin/splunk enable boot-start
+./opt/splunkforwarder/bin/splunk enable boot-start
 
 
 #Firewall Config
-sudo ufw enable
-sudo ufw allow 8080
-sudo ufw allow 8000
-sudo ufw allow 5514
-sudo ufw allow 9997
-sudo ufw reload
+ufw enable
+ufw allow 8080
+ufw allow 8000
+ufw allow 5514
+ufw allow 9997
+ufw reload
 
 
 #Splunk add monitoring logs
-sudo /opt/splunkforwarder/bin/splunk add forward-server 10.100.0.6:9997
-sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/auth.log
-sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/kern.log
-sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/lastlog
+./opt/splunkforwarder/bin/splunk add forward-server 10.100.0.6:9997
+./opt/splunkforwarder/bin/splunk add monitor /var/log/auth.log
+./opt/splunkforwarder/bin/splunk add monitor /var/log/kern.log
+./opt/splunkforwarder/bin/splunk add monitor /var/log/lastlog
 
 
 #Splunk Services Restart
-sudo /opt/splunkforwarder/bin/splunk restart
+./opt/splunkforwarder/bin/splunk restart
